198a2bbc863e1d2188b5bb88faff5363ac22b3a8
ruokei <modquack@gmail.com>
2020-08-30 22:02:06 +0800

    D-2-2

--- Assignment/src/OrderItem.java
+++ Assignment/src/OrderItem.java
@@ -7,48 +7,42 @@
  */
 
 
-public class OrderItem extends Product{
+public class OrderItem{
 
+	private Product product;
     private int quantity = 0;
     
-    public OrderItem(int quantity){ 
-    	this.quantity = quantity;
+    
+    public OrderItem(Product product){ 
+    	this.product = product;
     }
     
-    public OrderItem(String prodId, String prodName, String prodType, double price, int quantity){ 
-    	super(prodId, prodName, prodType, price);
-    	this.quantity = quantity;
+    public double getAmount(){
+    	return (product.getPrice() * quantity);
     }
     
     //Getter
-    
-    public Product getProduct(){
-    	return product;
-    }
 
     public int getQuantity() {
         return quantity;
     }
     
-    //Setter
-    public void setProduct(Product product) {
-        this.product = product;
+    public Product getProduct() {
+        return product;
     }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
+    
+    //Setter
+    
+    public void setProduct (Product product){
+    	this.product = product;
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
     
-    public double getAmount(){
-    	return (product.getPrice()*quantity);
-    }
-    
     public String toString(){
-    	return "\nItem: " + getProductDescription + "\nQuantity: " + quantity + "\nAmount: " + totalAmount;
+    	return "\nItem: " + product.getProdName() + "\nQuantity: " + quantity;
     }
 
     
