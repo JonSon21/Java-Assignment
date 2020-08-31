619044a6b59e55887718a99df273eb49aae02a2d
Marvinlim <62168437+Marvinlim@users.noreply.github.com>
2020-08-31 20:30:53 +0800

    m

---
+++ Assignment/src/ProductManagement.java
@@ -0,0 +1,15 @@
+package product;
+
+import product.ProductConsole; 
+/**
+ *
+ * @author Kevin Lim
+ */
+public class ProductManagement {
+    
+    public static void main(String[] args){
+        ProductConsole pc = new ProductConsole();
+        pc.start();
+    }
+    
+}
