154e3ab3831d5f59ae19c6aa8c1009324b822775
ruokei <modquack@gmail.com>
2020-09-02 18:58:33 +0800

    D-5-1

--- Assignment/src/Assignment.java
+++ Assignment/src/Assignment.java
@@ -29,13 +29,13 @@ public class Assignment {
 
         // Initialize all filepath
         String[] filepath = new String[7];
-        filepath[0] = "./obj/person.bin";
-        filepath[1] = "./obj/managers.bin";
-        filepath[2] = "./obj/branch.bin";
-        filepath[3] = "./obj/employees.bin";
-        filepath[4] = "./obj/product.bin";
-        filepath[5] = "./obj/orderItem.bin";
-        filepath[6] = "./obj/orderLists.bin";
+        filepath[0] = "../obj/person.bin";
+        filepath[1] = "../obj/managers.bin";
+        filepath[2] = "../obj/branch.bin";
+        filepath[3] = "../obj/employees.bin";
+        filepath[4] = "../obj/product.bin";
+        filepath[5] = "../obj/orderItem.bin";
+        filepath[6] = "../obj/orderLists.bin";
 
         try {
             FileInputStream[] fis = new FileInputStream[filepath.length];
