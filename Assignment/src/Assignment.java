5bd68c0405a5fa6dc3a9244d548266a7e18bb29b
ruokei <modquack@gmail.com>
2020-09-02 23:46:09 +0800

    D-5-4

--- Assignment/src/Assignment.java
+++ Assignment/src/Assignment.java
@@ -1,5 +1,9 @@
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
 
 public class Assignment{
