a84e96397ca51680be4525c311c801108352abc3
luncoming <62168449+luncoming@users.noreply.github.com>
2020-09-07 20:37:35 +0800

    y

--- Assignment/src/logout.java
+++ Assignment/src/logout.java
@@ -1,23 +1,21 @@
-import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Logout {
-	
-	// Variables
     private String username;
 
-	// Reset the username
     public Logout() {
         this.username = "";
     }
 
     // Logout time
     public String currentTime() {
-    	
         Date now = new Date();
+
         SimpleDateFormat formatTime = new SimpleDateFormat("hh.mm aa");
+
         String loginTime = formatTime.format(now);
-        
+
         return loginTime;
     }
 
